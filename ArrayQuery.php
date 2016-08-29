711841fa2895d43bca0dfd2c264b546ccc0227a1
igor-chepurnoi <igorzfort@gmail.com>
2016-08-29 18:11:13 +0300

    init

---
+++ ArrayQuery.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace yii2mod\query;
+
+use Yii;
+use yii\base\Component;
+use yii\db\QueryTrait;
+
+class ArrayQuery extends Component
+{
+    use QueryTrait;
+
+    /**
+     * @var string name of the data key, which should be used as row unique id - primary key.
+     */
+    public $primaryKeyName = 'id';
+
+    /**
+     * @var array the data to search, filter.
+     */
+    public $from;
+
+    /**
+     * @var string the class for processing the queries
+     */
+    public $queryProcessorClass = 'app\components\QueryProcessor';
+
+    /**
+     * @var QueryProcessor
+     */
+    protected $queryProcessor;
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        $this->queryProcessor = Yii::createObject($this->queryProcessorClass);
+
+        parent::init();
+    }
+
+    /**
+     * Executes the query and returns all results as an array.
+     *
+     * @return array
+     */
+    public function all()
+    {
+        $rows = $this->fetchData();
+
+        return $this->populate($rows);
+    }
+
+    /**
+     * Executes the query and returns a single row of result.
+     *
+     * @return bool|mixed
+     */
+    public function one()
+    {
+        $rows = $this->fetchData();
+
+        return empty($rows) ? false : reset($rows);
+    }
+
+    /**
+     * Returns the number of records.
+     *
+     * @return int
+     */
+    public function count()
+    {
+        $data = $this->fetchData();
+
+        return count($data);
+    }
+
+    /**
+     * Returns a value indicating whether the query result contains any row of data.
+     *
+     * @return bool
+     */
+    public function exists()
+    {
+        $data = $this->fetchData();
+
+        return !empty($data);
+    }
+
+    /**
+     * Sets data to be selected from.
+     *
+     * @param array $data
+     * @return $this
+     */
+    public function from(array $data)
+    {
+        $this->from = $data;
+
+        return $this;
+    }
+
+    /**
+     * Fetches data.
+     *
+     * @return mixed
+     */
+    protected function fetchData()
+    {
+        return $this->queryProcessor->process($this);
+    }
+
+    /**
+     * Converts the raw query results into the format as specified by this query.
+     *
+     * @param $rows
+     * @return array
+     */
+    public function populate($rows)
+    {
+        $result = [];
+
+        if ($this->indexBy === null) {
+            return array_values($rows); // reset storage internal keys
+        }
+
+        foreach ($rows as $row) {
+            if (is_string($this->indexBy)) {
+                $key = $row[$this->indexBy];
+            } else {
+                $key = call_user_func($this->indexBy, $row);
+            }
+            $result[$key] = $row;
+        }
+
+        return $result;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# yii2-array-query
-Yii2 component that allows for searching/filtering the elements of an array.
+ArrayQuery Component for Yii2
+=============================
+
+Allows searching/filtering of an array. This component is very useful when displaying array data in GridViews with an
+ArrayDataProvider.
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist yii2mod/yii2-array-query "*"
+```
+
+or add
+
+```json
+"yii2mod/yii2-array-query": "*"
+```
+
+to the require section of your composer.json.
+
+Querying Data
+-------------
+
+You may execute complex query on the array data using [[\yii2mod\query\ArrayQuery]] class. This class works similar to regular [[\yii\db\Query]] and uses same syntax. For example:
+
+```php
+$data = [
+    [
+        'id' => 1,
+        'username' => 'admin',
+        'email' => 'admin@example.com'
+    ],
+    [
+        'id' => 2,
+        'name' => 'test',
+        'username' => 'test@example.com'
+    ],
+];
+
+$query = new ArrayQuery();
+$query->from($data);
+$query->where(['username' => 'admin']);
+
+$rows = $query->all();
+```
