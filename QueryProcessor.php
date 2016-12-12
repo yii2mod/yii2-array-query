8c43b5357ccf1fb5b27f29cd27de52f9d538b81d
Igor Chepurnoy <igorzfort@gmail.com>
2016-12-13 00:39:42 +0200

    update php-cs-fixer version

--- QueryProcessor.php
+++ QueryProcessor.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @link https://github.com/yii2tech
+ * @see https://github.com/yii2tech
  *
  * @copyright Copyright (c) 2015 Yii2tech
  * @license [New BSD License](http://www.opensource.org/licenses/bsd-license.php)
